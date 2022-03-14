8654a9ee75c71fb9456c39d4d5d8021547ec2831
luiggy macias <ferrinluiggy@gmail.com>
2022-03-14 16:31:23 -0500

    dfd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], // forRoot() is used for the root module to configure the router.
+  exports: [RouterModule], // export the router directives to be available in throughout the app.
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
