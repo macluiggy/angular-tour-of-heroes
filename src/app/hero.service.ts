5b104aa3588e1dce2dfee4f669c96b3afd68d71a
luiggy macias <ferrinluiggy@gmail.com>
2022-03-15 10:32:53 -0500

    addind navigation routes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,8 +11,8 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHerores(): Observable<Hero[]> { // la version de Observable espera a que se emita el array de heroes, osea que es asincrono, una vez que se emite el array, se ejecuta el subscribe, el cual devuelve un observable que es el array de heroes. es como si un camion espera por unos pasajeros, cuando llegan, esto los lleva a otro lado y luego se bajan, el camion seria el Observable, subscribe es cuando los lleva a su destino.
-    const heroes = of(HEROES);
+  getHerores(): Observable<Hero[]> { // la version de Observable espera a que se emita el array de heroes, osea que es asincrono, una vez que se emite el array, se ejecuta el subscribe, el cual devuelve un observable que es el array de heroes. es como si un camion espera por unos pasajeros, cuando llegan, esto los lleva a otro lado y luego se bajan, el camion seria el Observable, subscribe es cuando los lleva a su destino y Hero[] son los pasajeros.
+    const heroes = of(HEROES); // of es una funcion que devuelve un observable, es como si fuera una promesa, pero no es una promesa, es un observable.
     this.messageService.add('HeroService: fetched heroes');
     return heroes
   }
